e69465559127e5aaf8bf94c9c28cff6fa4b5dfcc
Mrgt-0 <trita706@mail.ru>
2026-05-21 23:49:38 +0300

    add webhooks for deploing

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
-TOKEN = '8791874608:AAHR8eznndONzBBhQiDfX1mHPnozO4DdLQc'
+import os
+
+TOKEN = '8791874608:AAHhR0Z36CAOEN_aLvQ-PXM20btxmxL25a0'
 ADMIN_IDS = [806709593, 595795530, 1576242455]
 PHONE = "+79674317119"
 BANK = "Сбербанк"
 #TEST_GROUP_ID = -1001628595679
 #GROUP_ID = TEST_GROUP_ID // убрала чтобы не спамить сообщениями в группу. Потом вернуть!
 ANNOUNCE_TOPIC_ID = 5912
+
+WEBHOOK_URL = os.environ.get("https://mafiabot-test-oh6l.onrender.com")

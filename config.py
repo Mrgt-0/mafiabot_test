20ebebd8882077e1547f55537c3dc84880094351
Mrgt-0 <trita706@mail.ru>
2026-05-22 19:51:02 +0300

    mmm

--- config.py
+++ config.py
@@ -4,8 +4,11 @@ TOKEN = '8791874608:AAHhR0Z36CAOEN_aLvQ-PXM20btxmxL25a0'
 ADMIN_IDS = [806709593, 595795530, 1576242455]
 PHONE = "+79674317119"
 BANK = "Сбербанк"
-#TEST_GROUP_ID = -1001628595679
-#GROUP_ID = TEST_GROUP_ID // убрала чтобы не спамить сообщениями в группу. Потом вернуть!
+TEST_GROUP_ID = -1001628595679
+GROUP_ID = TEST_GROUP_ID 
 ANNOUNCE_TOPIC_ID = 5912
-
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://postgres:Test1TestMafia@db.udhsmhlhzdkzsrwrhalp.supabase.co:5432/postgres"
+)
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL", "https://mafiabot-test-oh6l.onrender.com")
